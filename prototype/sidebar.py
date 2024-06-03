2129a6246f2f31d897856de4d38b2f6cdbe16716
Victor Mulyungi <127780478+mulyung1@users.noreply.github.com>
2024-06-03 14:37:37 +0300

    Update sidebar.py
    sidebar update for dropdown menu

--- prototype/sidebar.py
+++ prototype/sidebar.py
@@ -5,7 +5,7 @@ def create_sidebar():
     sidebar = html.Div(
         [
             html.Div(
-                html.H3(
+                html.H1(
                     [
                         html.Span('Earth', style={'font-weight': 'bold', 'margin-left': 20, 'color': '#4a5d23'}),
                         html.Br(),
@@ -28,7 +28,7 @@ def create_sidebar():
                                 href='/',  # root path i.e to loading page
                                 style={
                                     # 'color': 'black',
-                                    'fontSize': 25
+                                    'fontSize': 20
                                 },
                             )
                         ),
@@ -36,88 +36,97 @@ def create_sidebar():
                             dbc.NavLink(
                                 [
                                     html.I(className='fa-solid fa-folder-open'),
-                                    'About',  # text beside icon
+                                    'Project Details',  # text beside icon
                                 ],
                                 href='/about',
                                 style={
                                     # 'color': 'black',
-                                    'fontSize': 25
+                                    'fontSize': 19
                                 },
                             )
                         ),
-                        dbc.NavItem(
-                            dbc.NavLink(
-                                [
-                                    html.I(className='fa-solid fa-window-restore'), 
-                                    'Store',  # text beside icon
-                                ],
-                                href='/store',
-                                style={
-                                    # 'color': 'black',
-                                    'fontSize': 25
-                                },
-                            )
-                        ),
-                        dbc.NavItem(
-                            dbc.NavLink(
-                                [
-                                    html.I(className='fa-solid fa-circle-question'), 
-                                    'FAQs',  # text beside icon
-                                ],
-                                href='/faqs',
-                                style={
-                                    # 'color': 'black',
-                                    'fontSize': 25
-                                },
-                            )
-                        ),
-                        dbc.NavItem(
-                            dbc.NavLink(
-                                [
-                                    html.I(className='fa-brands fa-github'), 
-                                    'GitHub',  # text beside icon
-                                ],
-                                href='https://github.com/mulyung1',
-                                target='_blank_',  # applies to external links only
-                                style={
-                                    # 'color': 'black',
-                                    'fontSize': 25
-                                },
-                            )
-                        ),
-                        dbc.NavItem(
-                            dbc.NavLink(
-                                [
-                                    html.I(className='fa-brands fa-medium'), 
-                                    'Medium',  # text beside icon
-                                ],
-                                href='https://medium.com/@victorsonofmartin',
-                                target='_blank_',
-                                style={
-                                    # 'color': 'black',
-                                    'fontSize': 25
-                                },
-                            )
-                        ),
+                        # dbc.NavItem(
+                        #     dbc.NavLink(
+                        #         [
+                        #             html.I(className='fa-solid fa-window-restore'), 
+                        #             'Store',  # text beside icon
+                        #         ],
+                        #         href='/store',
+                        #         style={
+                        #             # 'color': 'black',
+                        #             'fontSize': 20
+                        #         },
+                        #     )
+                        # ),
+                        # dbc.NavItem(
+                        #     dbc.NavLink(
+                        #         [
+                        #             html.I(className='fa-solid fa-circle-question'), 
+                        #             'FAQs',  # text beside icon
+                        #         ],
+                        #         href='/faqs',
+                        #         style={
+                        #             # 'color': 'black',
+                        #             'fontSize': 20
+                        #         },
+                        #     )
+                        # ),
+                        # dbc.NavItem(
+                        #     dbc.NavLink(
+                        #         [
+                        #             html.I(className='fa-brands fa-github'), 
+                        #             'GitHub',  # text beside icon
+                        #         ],
+                        #         href='https://github.com/mulyung1',
+                        #         target='_blank_',  # applies to external links only
+                        #         style={
+                        #             # 'color': 'black',
+                        #             'fontSize': 20
+                        #         },
+                        #     )
+                        # ),
+                        # dbc.NavItem(
+                        #     dbc.NavLink(
+                        #         [
+                        #             html.I(className='fa-brands fa-medium'), 
+                        #             'Medium',  # text beside icon
+                        #         ],
+                        #         href='https://medium.com/@victorsonofmartin',
+                        #         target='_blank_',
+                        #         style={
+                        #             # 'color': 'black',
+                        #             'fontSize': 20
+                        #         },
+                        #     )
+                        # ),
+                        dbc.DropdownMenu(
+                            label='Explore by country project',
+                            children=[
+                                dbc.DropdownMenuItem('store', href='/store'),
+                                dbc.DropdownMenuItem(divider=True),
+                                dbc.DropdownMenuItem('faqs', href='/faqs')
+                            ]
+                        )
                     ]
                 )
                 ], style={
                     #'backgroundColor':'#010103', 
-                    'height':350}
+                    'height':240}
             ),
             html.Div([
-                html.Img(src='assets/R.jpeg', style={'width': '100%', 'height': 'auto'})], 
-                    style={
-                        #'backgroundColor':'#010103', 
-                        'height':350,}
+                html.Img(src='assets/R.jpeg', style={'width': '100%', 'height': 'auto'})
+                ], 
+                style={
+                    #'backgroundColor':'#010103', 
+                    'height':300,}
             )
         ],
         style={
             'margin-left': 20,
             'margin-top': 20,
             'backgroundColor': '#ecebbd',
-            'height': 880,
-            'width': 300,
+            'height': 'calc(100vh - 60px)',
+            'width': '100%',
             'borderRadius': 6,
             'boxShadow': '0 4px 8px rgba(1, 0.9, 0.8, 0.9)'  # Add box shadow here
         }
